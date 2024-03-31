4b6b9a216c84a4c1743fc6c442faa556061c8e99
Yaroslav <yaro.04@mail.ru>
2024-03-31 15:03:14 +0300

    just random commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\basictable;
 
 class HomeController extends Controller
 {
@@ -13,6 +14,8 @@ class HomeController extends Controller
      */
     public function __construct()
     {
+        $table = basictable::all();
+
         $this->middleware('auth');
     }
     /**
