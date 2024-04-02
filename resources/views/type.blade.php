1e464547786f2a5bf6768a596b329769ce9144c1
Yaroslav <yaro.04@mail.ru>
2024-04-02 23:46:24 +0300

    added some night style for page

--- resources/views/type.blade.php
+++ resources/views/type.blade.php
@@ -1,4 +1,3 @@
-@vite(['resources/js/app.js'])
 <?php
 
 //Session set
@@ -94,14 +93,26 @@ echo "last try speed: {$outputSpeed} s/m <br>";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Auto Update Text</title>
+    <style>
+        body {
+            background-color: #1a202c;
+            color: #9ca3af;
+            input[type="text"], textarea {
 
-{{--    <script>--}}
-{{--        function updateText() {--}}
-{{--            alert("updateTextFunction");--}}
-{{--        }--}}
-{{--    </script>--}}
-    <!--    <script src='bootstrap-5.3.3-dist/js/bootstrap.min.js'></script>-->
-{{--    <link rel="stylesheet" href="bootstrap-5.3.3-dist/css/bootstrap.min.css">--}}
+                background-color : #577393;
+                border-color: #ef4444;
+            }
+            button {
+                background-color : #577393;
+                border-color: #ef4444;
+            }
+            input[type='submit'] {
+                background-color : #577393;
+                border-color: #ef4444;
+
+            }
+        }
+    </style>
 
 </head>
 <body>
